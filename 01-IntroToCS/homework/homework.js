e1320584e128782976b9ebce5a15e20cbc449e85
camiloduquee <camiloduquee@gmail.com>
2022-10-24 16:44:35 -0500

    ejercicio 1 resuelto

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,7 +2,14 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+// tu codigo aca
+let arr = num.split("").reverse();
+let NewArr = [];
+  for(let i = 0;i < arr.length; i++){
+    	NewArr[i] =arr[i] * Math.pow(2,i)
+  }
+ return NewArr.reduce(
+  (a, b) => a + b);
 }
 
 function DecimalABinario(num) {
