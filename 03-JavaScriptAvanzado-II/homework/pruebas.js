b7cafd063256db245fc5e1b8bda44b8a9c86e05b
camiloduquee <camiloduquee@gmail.com>
2022-10-27 22:46:20 -0500

    tarea terminada

--- 03-JavaScriptAvanzado-II/homework/pruebas.js
+++ 03-JavaScriptAvanzado-II/homework/pruebas.js
@@ -1,39 +1,4 @@
-function cacheFunction(cb) {
-
-  let cache = {};
-    
-  return function(arg){
-    if(cache.hasOwnProperty(arg)){
-      return cache[arg];
-    }
-    else{
-      cache[arg]=cb(arg);
-      return cache[arg];
-      
-    }
-    
-  };
- 
-}
-
-const cb = function(x) {
-  return x * 2;
-  
-};
-const cachedFunction = cacheFunction(cb);
-
-  var resultOne = cachedFunction(2);
-  var resultTwo = cachedFunction(3);
-  var resultThre = cachedFunction(10);
-
-  console.log(resultOne);
-  console.log(resultTwo);
-  console.log(resultThre);
-
-  console.log(cachedFunction(true));
-  console.log(cachedFunction(true));
-  console.log(cachedFunction(10));
-
+console
   
 
 
