ecb00de99572aa56a35f18410db6a7c4a6bd74a2
camiloduquee <camiloduquee@gmail.com>
2022-11-02 11:19:33 -0500

    realizado

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,38 +6,72 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-  var array = [1];
-  for (var i = 2; i < num+1; i++) {
-      if (num % i === 0) 
-          {
-            array.push(i);
-            num = num/i;
-            i=i-1;
-          }
-      }
-  return array;
+  // var array = [1];
+  // var operaciones = 0;
+  // for (var i = 2; i < num+1; i++) {
+  //     if (num % i === 0) 
+  //         {
+  //           array.push(i);
+  //           num = num/i;
+  //           i=i-1;
+  //           operaciones++;
+  //         }
+  //     }
+  //     console.log(operaciones);
+  // return array;
+
+// codigo de otra forma con un while
+
+ var factores = [1];
+ var i = 2;
+ while(num !== 1){
+     if(num % i === 0){
+       factores.push(i);
+       num = num / i;
+     }else{
+       i++;
+     }
+   }
+   return factores;
+
 }  
-//.log(factorear(2000000))
+console.log(factorear(180));
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-  for(var i = 0; i < array.length-1; i++){
+
+  // for(var i = 0; i < array.length-1; i++){
         
-      for(var x = 0; x < array.length-i; x++)
-        {
-        if(array[x] > array[x+1]){
-          var temp = array[x]; 
-            array[x] = array[x+1];
-            array[x+1] = temp;
-        }
-      }
+  //     for(var x = 0; x < array.length-i; x++)
+  //       {
+  //       if(array[x] > array[x+1]){
+  //         var aux = array[x]; 
+  //           array[x] = array[x+1];
+  //           array[x+1] = aux;
+  //       }
+  //     }
+  // }
+  // return array;
+
+//Compara de a dos elementos, el ultimo es el que ya queda como numero mayor
+var cambio = true; //mi bandera para saber si entra o no en el ciclo si es false ya deja de ejecutarce
+while(cambio){
+  cambio = false;
+  for (let i = 0; i < array.length - 1; i++) { //se poone el array.length - 1 porque debe analizar el penultimo numero con el ultimo 
+    if(array[i] > array[i + 1]){
+      var aux = array[i];
+      array[i] = array[i + 1];
+      array[i + 1] = aux;
+      cambio = true;
+    }
   }
-  return array;
 }
-//console.log(bubbleSort([5, 1, 4, 2, 8, 9, 6, 30,7,21,10,100,3,99,12,13,55,19]));
+return array;
+}
+console.log(bubbleSort([5, 1, 4, 2, 8, 9, 6, 30,7,21,10,100,3,99,12,13,55,19]));
 
 
 function insertionSort(array) {
@@ -45,19 +79,36 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-  for (let i = 1; i < array.length; i++) {
-    let j = i - 1
-    let temp = array[i]
-    while (j >= 0 && array[j] > temp) {
-       array[j + 1] = array[j]
-       j--
-    }
+//Ejercicio como lo planteo
+  // for (let i = 1; i < array.length; i++) {
+  //   let j = i - 1
+  //   let temp = array[i]
+  //   while (j >= 0 && array[j] > temp) {
+  //      array[j + 1] = array[j]
+  //      j--;
+  //   }
     
-    array[j+1] = temp
+  //   array[j+1] = temp
+  // }
+  // return array;
+//[1 , 2 , 3 , 4, 5]
+//        |i
+//    |j
+//otra manera de plantear el ejercicio ,recordar que arraca en la posicion 1
+for (let i = 1; i < array.length; i++){
+  var j = i - 1; //si arranco en en 1 entonces debo compararlo con el valor de antes osea j = i - 1
+  var aux = array[i];
+  while (j >= 0 && array[j] > aux){ //para que deje de ser true estas dos condiciones deben ser false
+    //Me voy corriendo a la izquierda
+    // Uso aux porque pise valor de el array[i]    
+    array[j +1] = array[j];
+        j--;
   }
-  return array;
+  array [j + 1] = aux;
 }
-//console.log(insertionSort([5, 1, 4, 2, 8, 9, 6, 30,7,21,10,10,3,99,12,13,55,19]));
+return array;
+}
+console.log(insertionSort([5, 1, 4, 2, 8, 9, 6, 30,7,21,10,10,3,99,12,13,55,19]));
 
 
 function selectionSort(array) {
@@ -65,19 +116,22 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-  for(let i = 0; i < array.length; i++) {
+  for(let i = 0; i < array.length - 1; i++) {
     // Finding the smallest number in the subarray
-    let Nmin = i;
-    for(let j = i+1; j < array.length; j++){
+  
+    var Nmin = i;
+    for(let j = i + 1; j < array.length; j++){ //for para encontrar el numero mas pequeño (indice)
         if(array[j] < array[Nmin]) {
           Nmin=j; 
         }
      }
-     if (Nmin != i) {
+     //Hago el swap
+     if (Nmin != i) {   
          // cambio el numero menor a la posicion
-         let tmp = array[i]; 
+         var aux = array[i]; 
          array[i] = array[Nmin];
-         array[Nmin] = tmp;      
+         array[Nmin] = aux;
+         
     }
 }
 return array;
